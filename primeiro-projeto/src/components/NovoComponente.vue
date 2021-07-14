8e7df2fb34dbbef14b66bdf6e23377f5e6d14f89
djenole <55561761+djenole@users.noreply.github.com>
2021-07-14 00:54:01 -0300

    Componente para exemplo de estrutura template

---
+++ primeiro-projeto/src/components/NovoComponente.vue
@@ -0,0 +1,21 @@
+/** O template requer apenas um elemento. Todos os conteúdos ficaram dentro de 
+uma div root, que será única em seu template */
+
+<template>
+    <div class="novo-componente">
+        <h1>Novo componente</h1>
+        <p>Teste</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'NovoComponente'
+}
+</script>
+
+<style scoped>
+
+
+</style>

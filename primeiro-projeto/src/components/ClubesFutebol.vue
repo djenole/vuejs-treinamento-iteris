56e5bbd13910acf52f4232b8649273e6534d66ad
djenole <55561761+djenole@users.noreply.github.com>
2021-07-14 01:41:18 -0300

    Consumo de api manipulando json

---
+++ primeiro-projeto/src/components/ClubesFutebol.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="clubes-futebol">
+        <h3>consumindo API</h3>
+        <ul>
+            <li v-for="site of sites" :key="site.id">
+                <a :href="site.link" >{{site.nome}} - {{site.pontos}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ClubesFutebol',
+    data() {
+        return {
+            sites: []
+        }
+    },
+
+    created() {
+        fetch('https://hackthon-decola.firebaseio.com/clubes-lista.json')
+            .then(resultado => resultado.json())
+            .then(json => {
+                this.sites = json;
+            })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

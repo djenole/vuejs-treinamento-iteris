403352981ab34a62dc5d5eb3655ea46b0355133f
djenole <55561761+djenole@users.noreply.github.com>
2021-07-14 02:09:18 -0300

    Criar dois métodos para manipular valor do contador

---
+++ primeiro-projeto/src/components/DesafioContador.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="desafio-contador">
+
+        <h3>Contador:{{contador}}</h3>
+
+        <button v-on:click="incrementar()">+</button>
+        <button v-on:click="decrementar()">-</button>
+      
+    </div>
+</template>
+    
+<script>
+export default {
+    name: 'DesafioContador',
+    data() {
+        return {
+            contador: 0
+        }
+    },
+
+    methods: {
+        incrementar() {
+            return this.contador++;
+        },
+        decrementar() {
+            return this.contador--;
+        }
+    }
+   
+    
+}
+</script>
+
+<style scoped>
+
+</style>

ea318f3ef762f319a6c964561740cc726b10543c
djenole <55561761+djenole@users.noreply.github.com>
2021-07-14 01:11:32 -0300

    Evento de click na frase

---
+++ primeiro-projeto/src/components/TesteEvento.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="teste-evento">
+        <h3> Teste evento </h3>
+        <p v-on:click="deixarMaiuscula">{{mensagem}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'TesteEvento',
+    data() {
+        return {
+            
+            mensagem:'Evento de click na frase'
+        }
+    },
+
+    methods: {
+        deixarMaiuscula() {
+            this.mensagem = this.mensagem.toUpperCase();
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>

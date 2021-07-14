b26532c8c6c6fa569dbce9d2f74113b6ea33db46
djenole <55561761+djenole@users.noreply.github.com>
2021-07-14 00:57:31 -0300

    Uso da diretiva v-if e v-else

---
+++ primeiro-projeto/src/components/TesteIf.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="teste-if"> 
+        <h3 v-if="podeVer">Teste de invisibilidade</h3>
+        <h3 v-else>Outro teste de invisibilidade </h3>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'TesteIf',
+    data() {
+        return {
+            podeVer: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

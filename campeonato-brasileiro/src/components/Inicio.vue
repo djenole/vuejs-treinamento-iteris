b4e247d527d4256631cbafb147ebe30e081eb072
djenole <55561761+djenole@users.noreply.github.com>
2021-07-15 16:13:02 -0300

    Criado o componente da pagina inicio

---
+++ campeonato-brasileiro/src/components/Inicio.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Inicio">
+        <h3>Inicio</h3>
+    </div>
+</template>
+
+<script>
+
+
+export default({
+    name: 'Inicio',
+    setup() {
+        
+    },
+})
+</script>
+
+
+<style scoped>
+
+</style>
